0c4ce6e1fc7717f927f4d15f330460075a8a881c
Avinash Jha <avinashjha19@outlook.com>
2025-03-01 20:48:36 +0530

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,45 @@
+// 
+
+
+
+
+
+
+
+import Link from "next/link";
+import { ModeToggle } from "./ModeToggle";
+import { CodeIcon } from "lucide-react";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import DasboardBtn from "./DasboardBtn";
+
+function Navbar() {
+  return (
+    <nav className="border-b bg-black backdrop-blur-md shadow-sm">
+      <div className="flex h-16 items-center px-6 lg:px-10 container mx-auto">
+        {/* LEFT SIDE - LOGO */}
+        <Link
+          href="/"
+          className="flex items-center gap-3 font-semibold text-2xl font-mono transition-transform duration-200 hover:scale-105"
+        >
+          <CodeIcon className="size-8 text-emerald-500" />
+          <span className="bg-gradient-to-r from-emerald-600 to-teal-500 bg-clip-text text-transparent">
+            Codexium
+          </span>
+        </Link>
+
+        {/* RIGHT SIDE - ACTIONS */}
+        <SignedIn>
+          <div className="flex items-center space-x-5 ml-auto">
+            <DasboardBtn />
+            <ModeToggle />
+            <div className="relative">
+              <UserButton afterSignOutUrl="/" />
+            </div>
+          </div>
+        </SignedIn>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
